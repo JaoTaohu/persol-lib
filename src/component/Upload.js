cbdc2e8ed472bf012ebf7693722360eb89fe6719
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2023-03-11 17:00:54 +0700

    success preview

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -1,6 +1,6 @@
-import { useRef, useEffect, useState, useContext } from 'react';
+import { useRef, useState, useContext } from 'react';
 import { storage } from '../config';
-import { ref, uploadBytes, listAll, getDownloadURL, deleteObject } from 'firebase/storage';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { v4 as uuid } from 'uuid';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from './auth';
@@ -11,7 +11,8 @@ function Upload() {
   const [imgList, setImgList] = useState([]);
   const imgListRef = useRef([]);
   const history = useHistory();
-  const { currentUser } = useContext(AuthContext)
+  const { currentUser } = useContext(AuthContext);
+  const [previewUrl, setPreviewUrl] = useState(null);
 
   const uploadImage = () => {
     if (img == null) return;
@@ -24,50 +25,20 @@ function Upload() {
       });
     });
     setImg(null);
-    
+    setPreviewUrl(null);
   };
 
-  useEffect(() => {
-    const imagesRef = ref(storage, `${currentUser.uid}/`);
-    listAll(imagesRef)
-      .then((res) => Promise.all(res.items.map((item) => getDownloadURL(item))))
-      .then((urls) => {
-        const sortedUrls = urls.sort((url1, url2) => {
-          // Sort in descending order of upload time
-          const index1 = url1.indexOf('?time=');
-          const index2 = url2.indexOf('?time=');
-          const time1 = parseInt(url1.substring(index1 + 6), 10);
-          const time2 = parseInt(url2.substring(index2 + 6), 10);
-          return time2 - time1;
-        });
-        imgListRef.current = sortedUrls; // Set the initial state of the list to the sorted URLs
-        setImgList(sortedUrls);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-
-  const deleteImage = (url) => {
-    const imgRef = ref(storage, url);
-    deleteObject(imgRef).then(() => {
-      const updatedImgList = imgList.filter((imgUrl) => imgUrl !== url);
-      imgListRef.current = updatedImgList;
-      setImgList(updatedImgList);
-    });
+  const handleImgChange = (event) => {
+    const file = event.target.files[0];
+    setImg(file);
+    setPreviewUrl(URL.createObjectURL(file));
   };
 
   return (
     <div className="App">
-      <input type='file' onChange={(event) => {setImg(event.target.files[0])}}/>
+      {previewUrl && <img src={previewUrl} alt="Preview" style={{ maxWidth: '40%', height: 'auto' }}/>}
+      <input type='file' onChange={handleImgChange}/>
       <button onClick={uploadImage} >Upload image</button>
-      {imgList.map((url) => {
-        return (
-          <div key={url}>
-            <img src={url} style={{ maxWidth: '40%', height: 'auto' }} />
-            <button onClick={() => deleteImage(url)}>Delete</button>
-          </div>
-        );
-      })}      
-
     </div>
   );
 }
